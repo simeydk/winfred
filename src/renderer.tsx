cac91790e3f4e78db0697f35c191079255c93d2a
Simey de Klerk <simeydeklerk@gmail.com>
2020-10-01 22:30:47 +0200

    React working

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -27,5 +27,11 @@
  */
 
 import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// document.getElementById('root').innerHTML = "js works"
+ReactDOM.render(<App />, document.getElementById('root'))
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
